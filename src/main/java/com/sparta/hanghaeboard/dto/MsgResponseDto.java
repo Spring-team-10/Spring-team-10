0978c2d6092692a7cba28ded481061d0fa5f8eb9
leejincha <leejincha@gmail.com>
2022-12-16 02:02:28 +0900

    예외처리 수정

--- src/main/java/com/sparta/hanghaeboard/dto/MsgResponseDto.java
+++ src/main/java/com/sparta/hanghaeboard/dto/MsgResponseDto.java
@@ -1,7 +1,11 @@
 package com.sparta.hanghaeboard.dto;
 
+import com.sparta.hanghaeboard.exception.CustomException;
+import com.sparta.hanghaeboard.exception.SuccessCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @Getter
 @Setter
@@ -19,5 +23,20 @@ public class MsgResponseDto { //전체 응답 Dto
     public MsgResponseDto(int statusCode, String msg) {
         this.statusCode = statusCode;
         this.msg = msg;
+    }
+
+    public MsgResponseDto(SuccessCode successCode) {
+        this.msg = successCode.getMessage();
+        this.statusCode = successCode.getHttpStatus().value();
+    }
+
+    public MsgResponseDto(CustomException customException) {
+        this.msg = customException.getMsg();
+        this.statusCode = customException.getStatuscode();
+    }
+
+    public MsgResponseDto(MethodArgumentNotValidException ex) {
+        this.msg = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        this.statusCode = HttpStatus.BAD_REQUEST.value();
     }
 }
--- src/main/java/com/sparta/hanghaeboard/exception/RestApiExceptionHandler.java
+++ src/main/java/com/sparta/hanghaeboard/exception/RestApiExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.sparta.hanghaeboard.exception;
 
+import com.sparta.hanghaeboard.dto.MsgResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,5 +20,15 @@ public class RestApiExceptionHandler {
                 restApiException,
                 HttpStatus.BAD_REQUEST
         );
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public MsgResponseDto customeException(final CustomException customException) {
+        return new MsgResponseDto(customException);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public MsgResponseDto processValidationError(MethodArgumentNotValidException ex) {
+        return new MsgResponseDto(ex);
     }
 }
